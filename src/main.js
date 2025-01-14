17d38b65e4b34d672262acd243a095acf9ebb812
Sima <sima.barati1372@gmail.com>
2025-01-14 18:07:09 +0330

    feat: init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
+import './assets/main.css'
+
+import {createApp} from 'vue'
 import App from './App.vue'
+import router from './router'
+import repositoryFactory from "./repositories/RepositoryFactory.js";
+import {createVuetify} from 'vuetify'
 
-Vue.config.productionTip = false
+const vuetify = new createVuetify({})
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App)
+app.provide("repositories", repositoryFactory)
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
